3bdb83f466271c660e1136615d23247e1ce65215
Steven Craig Moss <sc.moss@outlook.com>
2020-05-06 20:42:05 -0400

    Last commit

---
+++ stretch-assignment/digital_timer/index-retry.js
@@ -0,0 +1,9 @@
+//! Remember to save this as index.js or paste this work into index.js 
+
+/*
+TODO: counter variable declared and set to zero 
+TODO: timer digits all set to 0 
+TODO: setTimeout 
+*/
+const digits = document.getElementsByClassName('digit');
+console.log('HERE is the HTML Collection',digits)
